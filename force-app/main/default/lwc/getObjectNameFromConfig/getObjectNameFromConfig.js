6a83c26d104e228a20a0b6b0e6929834ac4c946c
Anand Narasimhan <anarasimhan@salesforce.com>
2020-05-15 14:41:00 -0400

    Added License headers

--- force-app/main/default/lwc/getObjectNameFromConfig/getObjectNameFromConfig.js
+++ force-app/main/default/lwc/getObjectNameFromConfig/getObjectNameFromConfig.js
@@ -1,3 +1,10 @@
+/* 
+ *  Copyright (c) 2018, salesforce.com, inc.
+ *  All rights reserved.
+ *  SPDX-License-Identifier: BSD-3-Clause
+ *  For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
 import { LightningElement,api,wire} from 'lwc';
 import getObjectApiName from '@salesforce/apex/AddChildObjectToConfigController.getObjectApiName';
 
--- force-app/main/default/lwc/timelineItemOtherObject/timelineItemOtherObject.js
+++ force-app/main/default/lwc/timelineItemOtherObject/timelineItemOtherObject.js
@@ -1,3 +1,10 @@
+/* 
+ *  Copyright (c) 2018, salesforce.com, inc.
+ *  All rights reserved.
+ *  SPDX-License-Identifier: BSD-3-Clause
+ *  For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
 import { LightningElement, api, track, wire } from 'lwc';
 import getTimelineItemChildData from '@salesforce/apex/RecordTimelineDataProvider.getTimelineItemChildData';
 import { loadScript } from 'lightning/platformResourceLoader';
